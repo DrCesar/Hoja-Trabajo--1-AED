abab587d8488d0cc3b0224162d8e2cf9a0b35cbd
ProMasterProgramar <vel15534@uvg.edu.gt>
2016-07-12 22:04:05 -0600

    Add files via upload

---
+++ Radio.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoradio;
+
+/**
+ *
+ * @author Marcel
+ */
+public class Radio {
+    int radio; 
+    String frecuencia;
+    String station;
+    
+    public void on() {  
+         
+    }     
+    
+    public void off (){
+    
+    }
+    
+    public void isOn() {
+    
+    }
+    
+    public String getFrecuency() {
+        
+        
+        return frecuencia; 
+    
+    }
+    
+    public void chageFrecuency(String frecuencia) {
+        this.frecuencia = frecuencia; 
+    
+    }
+    
+    public String getStation() {
+    
+        return station;
+    }
+    
+    public void setStation(String station){
+        this.station = station; 
+ 
+    }
+    
+    public void forward(){
+    
+    }
+    
+    public void backward(){
+    }
+}
